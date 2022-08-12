e5f7f26831eb0d9143a20716e2b1de62d7c6d5a0
Mister-Gee <Fakuadegbenga@gmail.com>
2022-08-12 14:17:46 +0100

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,38 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
+import axios from 'axios';
 import './App.css';
 
-function App() {
+const App: React.FC = () => {
+  const reqURL = "https://www.omdbapi.com/";
+  
+  const [searchInput, setSearchInput] = useState<string>("");
+  const [searchResult, setSearchResult] = useState<any>(undefined);
+
+
+  const search = () => {
+    if(searchInput === ""){
+      alert("Search Box is Empty");
+      return;
+    }
+
+    let query = searchInput.replace(/ /g, "+")
+    axios.get(`${reqURL}?t=${query}&apikey=dafc823c`).then((response) => {
+      setSearchResult(response.data)
+    });
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div style={{marginTop: '20px'}}>
+        <input value={searchInput} onChange={e => setSearchInput(e.target.value)} placeholder="Search By Movie Title" />
+        <button onClick={search} >Search</button>
+      </div>
+      {searchResult &&
+        <div style={{marginTop: '20px'}}>
+          <h3>Title: {searchResult?.Title}</h3>
+          <img src={searchResult?.Poster} alt=""/>
+        </div>
+      }
     </div>
   );
 }
